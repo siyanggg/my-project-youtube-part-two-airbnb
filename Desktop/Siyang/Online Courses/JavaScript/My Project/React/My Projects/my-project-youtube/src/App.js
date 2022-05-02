acfad28afc71a7ee34cdf953e4bbc423813ace77
SiYang Zhang <siyang.zhang@fdmgroup.com>
2022-05-02 13:22:57 +0800

    app.css edited

---
+++ Desktop/Siyang/Online Courses/JavaScript/My Project/React/My Projects/my-project-youtube/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+import Header from "./Component/Header";
+import Footer from ".Component/Footer";
+import Main from ".Component/Main";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/Siyang/Online Courses/JavaScript/My Project/React/My Projects/my-project-youtube/src/Component/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <main>
+      <h1 className="main--title">Fun facts about React</h1>
+      <ul classNamem="main--facts">
+        <li>Was first release in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+      </ul>
+    </main>
+  );
+}
