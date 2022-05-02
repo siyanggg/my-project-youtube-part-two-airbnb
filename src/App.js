aeebfa019642c39f72bb6336ad7dfdfa3bf04e70
SiYang Zhang <siyang.zhang@fdmgroup.com>
2022-05-02 16:07:27 +0800

    contact component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Card from "./components/Card";
+import Contact from "./components/Contact";
 
-export default function App() {
-  // <Hero />
+function App() {
   return (
-    <div>
-      <Navbar />
-      <Card />
+    <div className="contacts">
+      <Contact
+        img="./images/mr-whiskerson.png"
+        name="Mr. Whiskerson"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+      />
+      <Contact
+        img="./images/fluffykins.png"
+        name="Fluffykins"
+        phone="(212) 555-2345"
+        email="fluff@me.com"
+      />
+      <Contact
+        img="./images/felix.png"
+        name="Felix"
+        phone="(212) 555-4567"
+        email="thecat@hotmail.com"
+      />
+      <Contact
+        img="./images/pumpkin.png"
+        name="Pumpkin"
+        phone="(0800) CAT KING"
+        email="pumpkin@scrimba.com"
+      />
     </div>
   );
 }
+
+export default App;
