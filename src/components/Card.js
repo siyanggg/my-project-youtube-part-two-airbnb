95336c79610ad6c3c259d3e5c6fdc29f5acf5a06
SiYang Zhang <siyang.zhang@fdmgroup.com>
2022-05-03 01:11:54 +0800

    spread operator: ...items

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,28 +1,22 @@
 import React from "react";
 
 export default function Card(props) {
+  console.log(props);
   let badgeText;
-
   if (props.openSpots === 0) {
     badgeText = "SOLD OUT";
   } else if (props.location === "Online") {
     badgeText = "ONLINE";
   }
 
-  /*
-    Challenge:
-    1. Display the correct text in the badge based on the logic above
-    2. Only display the badge if badgeText has a value
-    */
-
   return (
     <div className="card">
-      {badgeText ? <div className="card--badge">{badgeText}</div> : null}
-      <img src={`../images/${props.img}`} className="card--image" />
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img src={`../images/${props.coverImg}`} className="card--image" />
       <div className="card--stats">
         <img src="../images/star.png" className="card--star" />
-        <span>{props.rating}</span>
-        <span className="gray">({props.reviewCount}) • </span>
+        <span>{props.stats.rating}</span>
+        <span className="gray">({props.stats.reviewCount}) • </span>
         <span className="gray">{props.location}</span>
       </div>
       <p className="card--title">{props.title}</p>
