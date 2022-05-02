e5e7303d9a344025cbcc8f40c0ef128bc12d36df
SiYang Zhang <siyang.zhang@fdmgroup.com>
2022-05-03 01:45:34 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from "react";
-import Header from "./components/Header";
-import Meme from "./components/Meme";
-/**
- * Challenge:
- * - Create a Meme component.
- * - Inside the Meme component, render a styled form
- *   with our 2 inputs and the button.
- * - Don't worry about adding any functionality yet
- */
+
 export default function App() {
+  function handleClick() {
+    console.log("I was clicked!");
+  }
+
+  function handleOnMouseOver() {
+    console.log("MouseOver");
+  }
+
+  /**
+   * Challenge:
+   * Log something to the console when the mouse hovers over the image
+   */
+
   return (
-    <div>
-      <Header />
-      <Meme />
+    <div className="container">
+      <img
+        src="https://picsum.photos/640/360"
+        onMouseOver={handleOnMouseOver}
+      />
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
