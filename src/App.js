53b2a3792d110fd7fc69fb9f6091566885ebd4a9
SiYang Zhang <siyang.zhang@fdmgroup.com>
2022-05-03 00:41:36 +0800

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -8,14 +8,23 @@ export default function App() {
   const cards = data.map((item) => {
     return (
       <Card
+        key={item.id}
         img={item.coverImg}
         rating={item.stats.rating}
         reviewCount={item.stats.reviewCount}
         location={item.location}
         title={item.title}
         price={item.price}
+        openSpots={item.openSpots}
       />
     );
   });
-  return <div>{cards}</div>;
+
+  // <Hero />
+  return (
+    <div>
+      <Navbar />
+      <section className="cards-list">{cards}</section>
+    </div>
+  );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,23 @@
 import React from "react";
 
 export default function Card(props) {
+  let badgeText;
+
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
+  /*
+    Challenge:
+    1. Display the correct text in the badge based on the logic above
+    2. Only display the badge if badgeText has a value
+    */
+
   return (
     <div className="card">
+      {badgeText ? <div className="card--badge">{badgeText}</div> : null}
       <img src={`../images/${props.img}`} className="card--image" />
       <div className="card--stats">
         <img src="../images/star.png" className="card--star" />
@@ -10,8 +25,8 @@ export default function Card(props) {
         <span className="gray">({props.reviewCount}) • </span>
         <span className="gray">{props.location}</span>
       </div>
-      <p>{props.title}</p>
-      <p>
+      <p className="card--title">{props.title}</p>
+      <p className="card--price">
         <span className="bold">From ${props.price}</span> / person
       </p>
     </div>
