db2320c26cf294ab596ea6d8e04218aa8a8a24b8
SiYang Zhang <siyang.zhang@fdmgroup.com>
2022-05-03 00:23:52 +0800

    cards using map

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "./data/data";
 
 export default function App() {
-  // <Hero />
-  return (
-    <div>
-      <Navbar />
+  const cards = data.map((item) => {
+    return (
       <Card
-        img="katie-zaferes.png"
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
       />
-    </div>
-  );
+    );
+  });
+  return <div>{cards}</div>;
 }
