448d052e96cca79faa8025f041b5b11e94ba7c3e
SiYang Zhang <siyang.zhang@fdmgroup.com>
2022-05-03 13:12:15 +0800

    preContact added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import { React, useState } from "react";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [contact, setContact] = useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (719) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: false,
+  });
+  /**
+   * Challenge: Fill in the values in the markup
+   * using the properties of our state object above
+   * (Ignore `isFavorite` for now)
+   */
 
-  function minus() {
-    setCount(function (oldValue) {
-      return oldValue - 1;
+  let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png";
+
+  function toggleFavourite() {
+    setContact((prevContact) => {
+      return {
+        ...preContact,
+        isFavourite: preContact.isFavorite,
+      };
     });
   }
 
-  function plus() {
-    setCount((prevCount) => prevCount + 1);
-  }
   return (
-    <div className="counter">
-      <button className="counter--minus" onClick={minus}>
-        –
-      </button>
-      <div className="counter--count">
-        <h1>{count}</h1>
-      </div>
-      <button className="counter--plus" onClick={plus}>
-        +
-      </button>
-    </div>
+    <main>
+      <article className="card">
+        <img src="./images/user.png" className="card--image" />
+        <div className="card--info">
+          <img
+            src={`..images/${starIcon}`}
+            className="card--favorite"
+            onClick={toggleFavorite}
+          />
+          <h2 className="card--name">
+            {contact.firstName}
+            {contact.lastName}
+          </h2>
+          <p className="card--contact">{contact.phone}</p>
+          <p className="card--contact">{contact.email}</p>
+        </div>
+      </article>
+    </main>
   );
 }
