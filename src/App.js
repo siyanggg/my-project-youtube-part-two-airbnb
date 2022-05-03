7bdd13cd9b3c46f5b1e0830b710a68b4cb2fa737
SiYang Zhang <siyang.zhang@fdmgroup.com>
2022-05-03 13:59:06 +0800

    child component added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React from "react";
-import Header from "./components/Header";
-import Meme from "./components/Meme";
-import memesData from "./data/memesData";
-/**
- * Challenge:
- * - Create a Meme component.
- * - Inside the Meme component, render a styled form
- *   with our 2 inputs and the button.
- * - Don't worry about adding any functionality yet
- */
+import { React, useState } from "react";
+import Count from "./components/Count";
+
 export default function App() {
-  // const memes = memesData.map((item) => {
-  //   return <Meme key={item.id} {...item} />;
-  // });
+  const [count, setCount] = useState(0);
+
+  function add() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function subtract() {
+    setCount((prevCount) => prevCount - 1);
+  }
+
+  console.log("App component rendered.");
+
   return (
-    <div>
-      <Header />
-      <Meme />
+    <div className="counter">
+      <button className="counter--minus" onClick={subtract}>
+        –
+      </button>
+      <Count number={count} />
+      <button className="counter--plus" onClick={add}>
+        +
+      </button>
     </div>
   );
 }
---
+++ src/components/Count.js
@@ -0,0 +1,10 @@
+import { React, useState } from "react";
+
+export default function Count(props) {
+  console.log("Count component rendered.");
+  return (
+    <div className="counter--count">
+      <h1>{props.number}</h1>
+    </div>
+  );
+}
