9680e77dfa89a3fc0af6acc87431d8543c37cffd
SiYang Zhang <siyang.zhang@fdmgroup.com>
2022-05-03 16:55:54 +0800

    added toggle shown function

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,11 +1,24 @@
-import React from "react";
+import { React, useState } from "react";
 
 export default function Joke(props) {
+  const [isShown, setIsShown] = useState(false);
+  const [showPunchline, setShowPunchline] = useState("Show Punchline");
+
+  function toggleShown() {
+    setIsShown((prevIsShown) => !prevIsShown);
+  }
+
+  function showOffPunchline() {
+    return isShown ? "Hide Punchline" : "Show Punchline";
+  }
+
   console.log(props.comments);
   return (
     <div>
       {props.setup ? <h3>Setup: {props.setup}</h3> : "No Setup."}
-      <p>{props.punchline}</p>
+      <p>{isShown && props.punchline}</p>
+      <button onClick={toggleShown}>{showOffPunchline()}</button>
+      <h3>{isShown}</h3>
       <hr />
     </div>
   );
