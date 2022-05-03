b4967ab4759dfd3717f93c92e102d479e95001c4
SiYang Zhang <siyang.zhang@fdmgroup.com>
2022-05-03 12:36:52 +0800

    useState added

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,13 @@
-import React from "react";
+import { React, useState } from "react";
 import memesData from "../data/memesData.js";
 
 export default function Meme(props) {
+  const [memeImage, setMemeImage] = useState("");
   function getMemeImage() {
     const memesArray = memesData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url;
+    setMemeImage(memesArray[randomNumber].url);
+    console.log("new image is: ", memesArray[randomNumber].url);
   }
 
   return (
@@ -17,6 +19,7 @@ export default function Meme(props) {
           Get a new meme image 🖼
         </button>
       </div>
+      <img src={memeImage} className="meme--image" />{" "}
     </main>
   );
 }
