cf3a44b0876d48a4df827924885d8b20776f2b9a
SiYang Zhang <siyang.zhang@fdmgroup.com>
2022-05-03 17:19:01 +0800

    number of messages vs message render

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Count from "./components/Count";
 
 export default function App() {
   const [count, setCount] = useState(0);
+  const [messages, setMessages] = useState(["a", "b"]);
 
   function add() {
     setCount((prevCount) => prevCount + 1);
@@ -15,14 +16,24 @@ export default function App() {
   console.log("App component rendered.");
 
   return (
-    <div className="counter">
-      <button className="counter--minus" onClick={subtract}>
-        –
-      </button>
-      <Count number={count} />
-      <button className="counter--plus" onClick={add}>
-        +
-      </button>
+    // <div className="counter">
+    //   <button className="counter--minus" onClick={subtract}>
+    //     –
+    //   </button>
+    //   <Count number={count} />
+    //   <button className="counter--plus" onClick={add}>
+    //     +
+    //   </button>
+    // </div>
+    <div>
+      {messages.length === 0 ? (
+        <h1>You are all caught up!</h1>
+      ) : (
+        <h1>
+          You have {messages.length} unread{" "}
+          {messages.length > 1 ? "messages" : "message"}
+        </h1>
+      )}
     </div>
   );
 }
