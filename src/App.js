abe891f348582aa8662e47271e21ed1ec22275f9
SiYang Zhang <siyang.zhang@fdmgroup.com>
2022-05-04 00:25:19 +0800

    useEffect []

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import { React, useEffect, useState } from "react";
 
 export default function App() {
   const [starWarsData, setStarWarsData] = useState({});
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
 
   // console.log("Component rendered");
 
   useEffect(() => {
-    fetch("https://swapi.dev/api/people/1")
+    console.log("Effect ran");
+    fetch(`https://swapi.dev/api/people/${count}`)
       .then((res) => res.json())
       .then((data) => setStarWarsData(data));
-  }, []);
+  }, [count]);
 
   return (
     <div>
-      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
       <h2>The count is {count}</h2>
       <button onClick={() => setCount((prevCount) => prevCount + 1)}>
-        Add
+        Get Next Character
       </button>
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
   );
 }
