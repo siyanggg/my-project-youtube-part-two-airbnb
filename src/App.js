7e6ffe24cff8e11e3c5d2ecce19b841813380c45
SiYang Zhang <siyang.zhang@fdmgroup.com>
2022-05-04 00:44:08 +0800

    useEffect added to meme generator

--- src/App.js
+++ src/App.js
@@ -1,76 +1,18 @@
 import { React, useState } from "react";
-
+import Header from "./components/Header";
+import Meme from "./components/Meme";
+/**
+ * Challenge:
+ * - Create a Meme component.
+ * - Inside the Meme component, render a styled form
+ *   with our 2 inputs and the button.
+ * - Don't worry about adding any functionality yet
+ */
 export default function App() {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-    passwordConfirm: "",
-    joinedNewsLetter: false,
-  });
-
-  function handleChange(event) {
-    const { name, value, type, checked } = event.target;
-    setFormData((prevFormData) => {
-      return {
-        ...prevFormData,
-        [name]: type === "checkbox" ? checked : value,
-      };
-    });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    if (formData.password === formData.passwordConfirm) {
-      console.log("Successfully signed up");
-    } else {
-      console.log("Passwords do not match");
-    }
-
-    if (formData.joinedNewsletter) {
-      console.log("Thanks for signing up for our newsletter!");
-    }
-  }
-
   return (
-    <div className="form-container">
-      <form className="form" onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email address"
-          className="form--input"
-          name="email"
-          onChange={handleChange}
-          value={formData.email}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          className="form--input"
-          name="password"
-          onChange={handleChange}
-          value={formData.password}
-        />
-        <input
-          type="password"
-          placeholder="Confirm password"
-          className="form--input"
-          name="passwordConfirm"
-          onChange={handleChange}
-          value={formData.passwordConfirm}
-        />
-
-        <div className="form--marketing">
-          <input
-            id="okayToEmail"
-            type="checkbox"
-            name="joinedNewsletter"
-            onChange={handleChange}
-            checked={formData.joinedNewsletter}
-          />
-          <label htmlFor="okayToEmail">I want to join the newsletter</label>
-        </div>
-        <button className="form--submit">Sign up</button>
-      </form>
+    <div>
+      <Header />
+      <Meme />
     </div>
   );
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,39 +1,80 @@
-import { React, useState } from "react";
-import memesData from "../data/memesData.js";
+import { React, useState, useEffect } from "react";
 
-export default function Meme(props) {
-  // const [memeImage, setMemeImage] = useState("http://i.imgflip.com/1bij.jpg");
+export default function Meme() {
+  /**
+   * Challenge:
+   * As soon as the Meme component loads the first time,
+   * make an API call to "https://api.imgflip.com/get_memes".
+   *
+   * When the data comes in, save just the memes array part
+   * of that data to the `allMemes` state
+   *
+   * Think about if there are any dependencies that, if they
+   * changed, you'd want to cause to re-run this function.
+   *
+   * Hint: for now, don't try to use an async/await function.
+   * Instead, use `.then()` blocks to resolve the promises
+   * from using `fetch`. We'll learn why after this challenge.
+   */
 
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
-  const [allMemeImages, setAllMemeImages] = useState(memesData);
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
 
   function getMemeImage() {
-    const memesArray = allMemeImages.data.memes;
-    const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url;
-    setMeme((prevMeme) => {
-      return {
-        ...prevMeme,
-        randomImage: url,
-      };
-    });
-    console.log("new image is: ", memesArray[randomNumber].url);
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
   }
 
   return (
     <main>
       <div className="form">
-        <input type="text" placeholder="Top text" className="form--input" />
-        <input type="text" placeholder="Bottom text" className="form--input" />
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button className="form--button" onClick={getMemeImage}>
           Get a new meme image 🖼
         </button>
       </div>
-      <img src={meme.randomImage} className="meme--image" />{" "}
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
