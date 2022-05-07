a5caae457ba49767bfac59378710482921f041d1
SiYang Zhang <siyang.zhang@fdmgroup.com>
2022-05-07 13:40:28 +0800

    counter moved to component (got error)

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,10 +2,30 @@ import { React, useState } from "react";
 
 export default function Counter(props) {
   const { number } = props;
+  const [count, setCount] = useState(0);
+
   console.log("Counter component rendered.");
+
+  function add() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function subtract() {
+    setCount((prevCount) => prevCount - 1);
+  }
+
+  function reset() {
+    setCount(0);
+  }
   return (
     <div className="counter--count">
-      <h3>The count is: {number}</h3>
+      <h1>Counter example:</h1>
+      <div>
+        <button onClick={add}>Add</button>
+        <button onClick={subtract}>Subtract</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+      <h3>The count is: {count}</h3>
     </div>
   );
 }
